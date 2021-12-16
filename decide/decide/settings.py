0ea953cbd488488b7c8a6ec6af85f12dd3a51b08
PepeCesarG <pepecesargarcia@gmail.com>
2021-12-16 22:18:02 +0100

    feat(#1): i18n working

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -157,7 +157,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -166,6 +166,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
