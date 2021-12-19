842e0fa794fffd8cae6cce38189add492e7981cf
jacgarvel <jacobo.estudiante16@fmail.com>
2021-12-19 19:10:10 +0100

    feat(#23): Añadido js al form de questions

---
+++ decide/voting/static/script.js
@@ -0,0 +1,13 @@
+console.log('Hola')
+
+var select = document.getElementById("id_type");
+var options = document.getElementById("options-group");
+
+select.addEventListener('change',(e)=>{
+    var value = e.target.value;
+    if(value === 'B'){
+        options.setAttribute('hidden','');
+    }else if (value === 'O'){
+        options.removeAttribute('hidden');
+    }
+});
