494ef20266c6f28019fe7394b81e358690e94e6f
jacgarvel <jacobo.estudiante16@fmail.com>
2022-01-02 15:06:17 +0100

    fix(#56):Eliminado el endpoint /api y modificado el de cabina

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.VotingFormView.as_view(), name='voting'),
-    path('api/', views.VotingView.as_view(), name='voting'),
+    path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
     path('success/', views.SuccessView.as_view(), name='voting'),
 ]
