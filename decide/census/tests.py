2b1dc5f7e8abe53053d4cee6b9c4a2de29b4704b
David <davbarsal1@alum.us.es>
2022-01-03 16:45:58 +0100

    Test/Feature: Añadida funcionalidad completa #34
    Se ha añadido la funcionalidad completa respecto a la automatización de la
    creacion de votantes, créandose censos asociados segun la edad, localidad
    o genero, ademas de crear un test con Selenium que comprueba esta funcionalidad

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -3,23 +3,27 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.test import TestCase
 from rest_framework.test import APIClient
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from .models import Census
 from voting.models import Voting, Question, QuestionOption
 from mixnet.models import Auth
 from django.contrib.auth.models import User
 from base import mods
 from base.tests import BaseTestCase
-from .models import Voter
+from .models import Voter,Census
 from .admin import CensusAdmin, VoterAdmin
 from django.core.exceptions import ObjectDoesNotExist
 import os
-
-
+import logging
+from selenium.webdriver.support.ui import Select
+import random
 class CensusTestCase(BaseTestCase):
     
     voter = None
-    voting = None
+    voting =None
     census = None
     
     def setUp(self):
@@ -57,46 +61,101 @@ class CensusTestCase(BaseTestCase):
         user.id = pk
         user.save()
         return user
-    
+        
+    #TEST QUE COMPRUEBA LA FEATURE #34 PARA EL CENSO: Automatizar el proceso de creacion de votos
+    #de tal manera que si se crea un votante con una localidad, edad y genero se creen los censos 
+    #respectivos, para hacer este test es necesario que se introduzca un usuario con permisos y tener 
+    #la base de datos limpia
     def test_create_check_censuss(self):
-        self.voting = self.create_voting()
-        self.voting.save()
-        try:
-            self.user = User.objects.get(username='testvoter{}'.format(1))
-        except ObjectDoesNotExist:
-            self.user = User.objects.create(username='testvoter{}'.format(1))
+        usuarioconpermisos = ''
+        contraseñaconpermisos = ''
+        if usuarioconpermisos=='' or contraseñaconpermisos=='':
+            logging.debug('NO SE HA INTRODUCIDO USUARIO O CONTRASEÑA EN EL TEST')
+        i = random.randint(0,100000)
+        logger = logging.getLogger('selenium.webdriver.remote.remote_connection') 
+        logger.setLevel(logging.WARNING)
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        driver = webdriver.Chrome(options=options)
+        driver.get("http://127.0.0.1:8081/admin")
+        username = driver.find_element_by_name('username')
+        password = driver.find_element_by_name('password')
+        username.send_keys(usuarioconpermisos)
+        password.send_keys(contraseñaconpermisos)
+        driver.find_element_by_id("login-form").submit()
+        #driver.get('http://127.0.0.1:8081/admin/auth/user/')
+        driver.get("http://127.0.0.1:8081/admin/auth/user/add/")
+        createuser = driver.find_element_by_name('username')
+        createpassword = driver.find_element_by_name('password1')
+        confirmpassword = driver.find_element_by_name('password2')
+        createuser.send_keys('usuarioprueba'+str(i))
+        createpassword.send_keys('contrasenaprueba1')
+        confirmpassword.send_keys('contrasenaprueba1')
+        driver.find_element_by_id('user_form').submit()
+        driver.get("http://127.0.0.1:8081/admin/census/voter/add")
+        selectuser = Select(driver.find_element_by_name('user'))
+        selectuser.select_by_visible_text('usuarioprueba'+str(i))
+        selectlocation = Select(driver.find_element_by_name('location'))
+        selectlocation.select_by_visible_text('Zaragoza')
+        insertedad = driver.find_element_by_name('edad')
+        insertedad.send_keys('45')
+        selectgenero = Select(driver.find_element_by_name('genero'))
+        selectgenero.select_by_visible_text('Hombre')
+        driver.find_element_by_id('voter_form').submit()
+        driver.get('http://127.0.0.1:8081/admin/census/census/')
+        censocreadoporgenero = driver.find_element_by_link_text('Hombre').text
+        censocreadoporlocation =driver.find_element_by_link_text('Sevilla').text
+        censocreadoporedad = driver.find_element_by_link_text('45').text
+        logging.debug('Censo por locacion:' + str(censocreadoporlocation))
+        logging.debug('Censo por genero:' + str(censocreadoporgenero))
+        logging.debug('Censo por edad:' + str(censocreadoporedad))
+        
+    def create_voter(self):
+        Voter.objects.all().delete()
+        self.user, _ = User.objects.get_or_create(username='testvoter{}'.format(1))
         self.user.is_active = True
         self.user.save()
-        try:
-            self.voter = Voter.objects.get(user=self.user)
-        except ObjectDoesNotExist:
-            self.voter = Voter.objects.create(user=self.user, location='Sevilla', edad='45',genero='Hombre')
+        self.voter, _ = Voter.objects.get_or_create(user=self.user, location='Avila', edad='50',genero='Mujer')
         self.voter.save()
-        self.censuss = Census.objects.all()
-        i =1
-        for self.c in self.censuss:
-            if i == 1:
-                self.assertEqual(self.c.name, 'Sevilla')
-            if i==2:
-                self.assertEqual(self.c.name, '45')
-            if i==3:
-                self.assertEqual(self.c.name,'Hombre')
-            i = i +1
-        return self.censuss
-            
+    def create_census(self):
+        Census.objects.all().delete()
+        Voting.objects.all().delete()
+        self.voting = self.create_voting()
+        self.voting.save()
+        self.create_voter()
+        self.census = Census(name="Avila")
+        self.census.id = 1
+        self.census.save()
+        self.census.voting_ids.add(self.voting)
+        self.census.save()
+        self.census.voter_ids.add(self.voter)
+        self.census.save()
+        self.census = Census(name="50")
+        self.census.id = 2
+        self.census.save()
+        self.census.voting_ids.add(self.voting)
+        self.census.save()
+        self.census.voter_ids.add(self.voter)
+        self.census.save()
+        self.census = Census(name="Mujer")
+        self.census.id = 3
+        self.census.save()
+        self.census.voting_ids.add(self.voting)
+        self.census.save()
+        self.census.voter_ids.add(self.voter)
+        self.census.save()
 
-    '''
+            
     def test_check_vote_permissions(self):
-        self.censuss = self.test_create_check_censuss()
-        self.census = self.censuss.objects.filter(name='Sevilla')
-        response = self.client.get('/census/{}/?voter_id={}'.format(self.voting.id, self.voting.id), format='json')
+        self.create_census()
+        response = self.client.get('/census/{}/?voter_id={}'.format(self.voting.id, 580), format='json')
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(), 'Invalid voter')
-
-        response = self.client.get('/census/{}/?voter_id={}'.format(self.voting.id, self.voter.id), format='json')
+        response = self.client.get('/census/{}/?voter_id={}'.format(self.voting.id, self.voter.user.id), format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), 'Valid voter')
-
+    
+    
     def test_list_voting(self):
         self.create_census()
         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
@@ -105,12 +164,11 @@ class CensusTestCase(BaseTestCase):
         self.login(user='noadmin')
         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
         self.assertEqual(response.status_code, 403)
-
         self.login()
         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'voters': [self.voter.id]})
-
+        self.assertEqual(response.json(), {'voters': [self.voter.id,self.voter.id,self.voter.id]})
+    
     def test_add_new_voters_conflict(self):
         self.create_census()
         data = {'name': 'prueba creacion', 'votings': [self.voting.id], 'voters': [self.voter.id]}
@@ -125,8 +183,11 @@ class CensusTestCase(BaseTestCase):
         response = self.client.post('/census/', data, format='json')
         self.assertEqual(response.status_code, 409)
     '''
-    '''def test_add_new_voters(self):
-        data = {'voting_id': 2, 'voters': [1,2,3,4]}
+    def test_add_new_voters(self):
+        self.create_census()
+        print('El voter a añadir es:'+ str(self.voter.id))
+        print('El voting a añadir es:'+ str(self.voting.id))
+        data = {'name':'prueba creacion','votings': [self.voting.id], 'voters': [self.voter.id]}
         response = self.client.post('/census/', data, format='json')
         self.assertEqual(response.status_code, 401)
 
@@ -134,19 +195,25 @@ class CensusTestCase(BaseTestCase):
         response = self.client.post('/census/', data, format='json')
         self.assertEqual(response.status_code, 403)
 
-        self.login()
+        self.login(user='admin',password='qwerty')
         response = self.client.post('/census/', data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)'''
+        self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)
+    
     '''
+    
     def test_destroy_voter(self):
         self.create_census()
+        
         data = 'test census'
-        response = self.client.delete('/census/{}/'.format(1), data, format='json')
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(0, Census.objects.count())
+        for c in Census.objects.all():
+            response = self.client.delete('/census/{}/'.format(c.id), data, format='json')
+            self.assertEqual(response.status_code, 204)
+            
+        
+    
     '''
-    '''
+    
     def test_csv_import_success(self):
         self.login()
         for i in range(1,5):
