bd91f23235144f22b72558a76210b63c5694aaa1
Jesus-RG <rivero.gallardo.jesus@gmail.com>
2022-01-03 18:31:37 +0100

    feat(#51): Añadidas imágenes y enlace a la vista
    Se han añadido imágenes explicativas y se ha introducido la vista en el sistema

--- decide/catalog/views.py
+++ decide/catalog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'homepage.html', {})
 
+def guia(request):
+    return render(request, 'guia.html', {})
+
--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
+    path('guia/', views.guia, name='guia'),
 ]
 
 for module in settings.MODULES:
