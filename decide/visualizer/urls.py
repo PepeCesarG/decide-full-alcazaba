4b06e0694155862930b145e9448950dbc7cf6e08
dansuaper <16dsp918@gmail.com>
2022-01-04 15:16:35 +0100

    feat(#10): Inclusion de boton para navegar al documento pdf

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('<int:voting_id>/graficos', VisualizerView2.as_view()),
-    path('<int:voting_id>/pdf', VisualizerView3.as_view()),
+    path('<int:voting_id>/pdf', VisualizerView3.as_view(), name = 'pdf'),
 ]
