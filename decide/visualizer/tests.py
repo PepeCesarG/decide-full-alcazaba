351431e3d042883f1cf0b50e741f31fbc69c6c4a
fraviltor <fvillazan1999@gmail.com>
2022-01-06 12:15:40 +0100

    test(#101): Test de redirección al módulo de Administración
    Test que comprueba la redirección e inicio de sesión del administrador.

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# Create your tests here.
+class TestRedirectAPI():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+  
+    def teardown_method(self, method):
+        self.driver.quit()
+    
+    def test_redirectAdmin(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://localhost:8081/")
+        element = self.driver.find_element(By.CSS_SELECTOR, ".imagen-port:nth-child(1) > .hover-galeria")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.LINK_TEXT, "admin/")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        self.driver.find_element(By.LINK_TEXT, "admin/").click()
+        self.driver.find_element(By.ID, "content").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("fraviltor")
+        self.driver.find_element(By.ID, "id_password").send_keys("fraviltor")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        assert self.driver.find_element(By.LINK_TEXT, "Administración de Django").text == "Administración de Django"
+        assert self.driver.find_element(By.CSS_SELECTOR, "strong").text == "FRAVILTOR"
