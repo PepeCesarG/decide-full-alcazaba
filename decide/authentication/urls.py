0c8c97eb82138bfcdbbf35bf1861a4b326a6982c
mrivas10 <manolo.rivas.llamas@gmail.com>
2022-01-07 14:08:25 +0100

    Tarea formateo visual registro de usuario completada, pero no funciona el registro

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,17 +1,22 @@
 from django.urls import include, path
+from django.contrib.auth.views import logout
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView, RegisterView,SigninView,ProfileView,SignupView,ListUsers,GetUserDetails
+from .views import *
 
 
 urlpatterns = [
+  
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
-    path('register/', RegisterView.as_view()),
-    path('sign-in/', SigninView.as_view()),
-    path('sign-up/', SignupView.as_view()),
-    path('profile/', ProfileView.as_view(), name="profile"),
-    path('getallusers/', ListUsers.as_view()),
-    path('getuserdetails/<int:user_id>/', GetUserDetails.as_view()),
+    path('decide/login/', LoginUserView.as_view(), name='auth_login'),
+    path('decide/logout/', logout, name="auth_logout"),
+    path('decide/register/', RegisterUserView.as_view(), name="auth_register"),
+    path('decide/register/complete/', CompleteVotingUserDetails.as_view(), name='auth_register_complete'),
+    path('decide/getVotingUser/', GetVotingUser.as_view(), name='auth_get_voting_user'),
+    path('decide/getGenresByIds/', GetGenresByIds.as_view(), name='auth_get_voting_user_genre'),
+   
+    path('user/<int:id>/', GetUserDetailsView.as_view()),
+   
 ]
