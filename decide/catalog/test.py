fedbb581ab930533e32a3d2888aa86d49e6f1f3e
fraviltor <fvillazan1999@gmail.com>
2022-01-07 17:08:23 +0100

    fix(#114) Error moduleNotFound en los tests

--- decide/catalog/test.py
+++ decide/catalog/test.py
@@ -34,7 +34,7 @@ class VisualizerTestCase(BaseTestCase):
     def tearDown(self):
         self.driver.quit()
         super().tearDown()
-
+    
     def test_get_home_ok(self):
         self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/")
         assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "Vota en Decide"
@@ -91,4 +91,31 @@ class VisualizerTestCase(BaseTestCase):
         url = self.driver.current_url
         assert url == "https://decide-full-alcazaba-visualize.herokuapp.com/visualizer/5/"
 
-        
+    def test_redirectAdmin(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/")
+        element = self.driver.find_element(By.CSS_SELECTOR, ".imagen-port:nth-child(1) > .hover-galeria")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.LINK_TEXT, "admin/")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        self.driver.find_element(By.LINK_TEXT, "admin/").click()
+        self.driver.find_element(By.ID, "content").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").send_keys("buenas1234")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        assert self.driver.find_element(By.LINK_TEXT, "Administración de Django").text == "Administración de Django"
+        assert self.driver.find_element(By.CSS_SELECTOR, "strong").text == "ADMIN"
+    
+    def test_incorrectAdmin(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/admin/login/?next=/admin/")
+        self.driver.set_window_size(909, 1016)
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("incorrecto")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".errornote")
+        assert len(elements) > 0
