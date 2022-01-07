376d432549ef7a991ab8e2eaa9e4ae7cc9ea0cc6
Jesus-RG <rivero.gallardo.jesus@gmail.com>
2022-01-07 17:37:53 +0100

    fix(#115): Arreglo de tests en común
    Puesta en común de los tests de visualizer, además de mover los tests correspondientes a catalog y arreglar los errores de importaciones

--- decide/catalog/test.py
+++ decide/catalog/test.py
@@ -144,26 +144,28 @@ class VisualizerTestCase(BaseTestCase):
         self.driver.find_element(By.LINK_TEXT, "postproc/").click()
         assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "Post Proc"
 
-    def test_access_visualizer_200(self):
+    def test_view_url_guia_error(self):
+        resp = self.client.get('/guiaUsuario')
+        self.assertEqual(resp.status_code, 404)
+     
+    def test_guia_ok(self):
         self.driver = webdriver.Chrome()
-        response = self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/visualizer/5")
-        assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Votación no comenzada"
-
-    def test_darkmode(self):
-        self.driver = webdriver.Chrome()
-        response = self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/visualizer/5")
-        self.driver.find_element(By.CSS_SELECTOR, "span:nth-child(1)").click()
-        '''
-        assert self.driver.find_element(By.CSS_SELECTOR, ".text-muted > th:nth-child(1)").text == "Opción"
-        assert self.driver.find_element(By.CSS_SELECTOR, "th:nth-child(2)").text == "Puntuación"
-        assert self.driver.find_element(By.CSS_SELECTOR, "th:nth-child(3)").text == "Votos"
-        '''
-        assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Votación no comenzada"
+        self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/guia/")
+        assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "Manual de uso de nuevas funcionalidades"
     
-    def test_lightmode(self):
+    def test_guiaInicio(self):
         self.driver = webdriver.Chrome()
-        response = self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/visualizer/5")
-        self.driver.find_element(By.CSS_SELECTOR, "span:nth-child(1)").click()
-        assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Votación no comenzada"
-        self.driver.find_element(By.CSS_SELECTOR, "span:nth-child(2)").click()
-        assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Votación no comenzada"
+        self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/guia/")
+        self.driver.find_element(By.LINK_TEXT, "Inicio").click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".galeria-port")
+        assert len(elements) > 0
+    
+    def test_guiaAdmin(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://decide-full-alcazaba-visualize.herokuapp.com/guia/")
+        self.driver.find_element(By.LINK_TEXT, "Admin").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").send_keys("buenas1234")
+        self.driver.find_element(By.CSS_SELECTOR, ".submit-row > input").click()
+        assert self.driver.find_element(By.LINK_TEXT, "Administración de Django").text == "Administración de Django"
+        assert self.driver.find_element(By.CSS_SELECTOR, "strong").text == "ADMIN"
